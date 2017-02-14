b8166c33a2210b77914ba74b58468515e3560de1
Impler <704090860@qq.com>
2017-02-14 21:42:12 +0800

    LeetCode Q1 and Q2

---
+++ LeetCode/src/main/java/com/study/leetcode/Q1.java
@@ -0,0 +1,23 @@
+package com.study.leetcode;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+ * You may assume that each input would have exactly one solution, and you may not use the same element twice.
+ * Example:
+ * Given nums = [2, 7, 11, 15], target = 9,
+ * Because nums[0] + nums[1] = 2 + 7 = 9,
+ * return [0, 1].
+ * Subscribe to see which companies asked this question.
+ */
+public class Q1 {
+	
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}

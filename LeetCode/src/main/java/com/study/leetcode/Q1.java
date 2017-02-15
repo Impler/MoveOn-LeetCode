26309ae93ebf2219fda20599db08ccab9c16dc70
Impler <704090860@qq.com>
2017-02-15 11:05:40 +0800

    solve Q1 and Q2

--- LeetCode/src/main/java/com/study/leetcode/Q1.java
+++ LeetCode/src/main/java/com/study/leetcode/Q1.java
@@ -1,23 +1,64 @@
 package com.study.leetcode;
 
-import static org.junit.Assert.*;
+import java.util.Arrays;
+import java.util.HashMap;
 
 import org.junit.Test;
 
 /**
- * Given an array of integers, return indices of the two numbers such that they add up to a specific target.
- * You may assume that each input would have exactly one solution, and you may not use the same element twice.
- * Example:
- * Given nums = [2, 7, 11, 15], target = 9,
- * Because nums[0] + nums[1] = 2 + 7 = 9,
- * return [0, 1].
- * Subscribe to see which companies asked this question.
+ * Given an array of integers, return indices of the two numbers such that they
+ * add up to a specific target. You may assume that each input would have
+ * exactly one solution, and you may not use the same element twice. Example:
+ * Given nums = [2, 7, 11, 15], target = 9, Because nums[0] + nums[1] = 2 + 7 =
+ * 9, return [0, 1]. Subscribe to see which companies asked this question.
+ * 
+ * public int[] twoSum(int[] nums, int target) {
+ * 
+ * }
  */
 public class Q1 {
-	
-	@Test
-	public void test() {
-		fail("Not yet implemented");
+
+	public static int[] twoSumByHashMapWithTwoLoop(int[] nums, int target) {
+		if (null == nums || nums.length == 0) {
+			return null;
+		}
+		HashMap<Integer, Integer> buffMap = new HashMap<Integer, Integer>();
+		for (int i = 0; i < nums.length; i++) {
+			buffMap.put(nums[i], i);
+		}
+		for (int i = 0; i < nums.length; i++) {
+			Integer anotherNumIndex = buffMap.get(target - nums[i]);
+			if (null != anotherNumIndex && anotherNumIndex != i) {
+				return new int[] { i, anotherNumIndex };
+			}
+		}
+		return null;
 	}
 
+	public static int[] twoSumByHashMapWithOneLoop(int[] nums, int target) {
+		if (null == nums || nums.length == 0) {
+			return null;
+		}
+		HashMap<Integer, Integer> buffMap = new HashMap<Integer, Integer>();
+		for (int i = 0; i < nums.length; i++) {
+			Integer anotherNumIndex = buffMap.get(target - nums[i]);
+			if (null == anotherNumIndex) {
+				buffMap.put(nums[i], i);
+			}else{
+				return new int[]{anotherNumIndex, i};
+			}
+		}
+		return null;
+	}
+	@Test
+	public void testTwoSumByHashMapWithTwoLoop() {
+		int[] results = Q1.twoSumByHashMapWithTwoLoop(new int[] { 7, 3, 4, 2, 9, 5 }, 14);
+		System.out.print(Arrays.toString(results));
+	}
+	
+	@Test
+	public void testTwoSumByHashMapWithOneLoop() {
+		int[] results = Q1.twoSumByHashMapWithOneLoop(new int[] { 7, 3, 4, 2, 9, 5 }, 14);
+		System.out.print(Arrays.toString(results));
+	}
 }
--- LeetCode/src/main/java/com/study/leetcode/Q2.java
+++ LeetCode/src/main/java/com/study/leetcode/Q2.java
@@ -1,7 +1,5 @@
 package com.study.leetcode;
 
-import static org.junit.Assert.*;
-
 import org.junit.Test;
 
 /**
@@ -10,12 +8,15 @@ import org.junit.Test;
  * contain a single digit. Add the two numbers and return it as a linked list.
  * You may assume the two numbers do not contain any leading zero, except the
  * number 0 itself. 
- * Input: (2 -> 4 -> 3) + (5 -> 6 -> 4) 
+ * Input: (2 -> 4 -> 3) 
+ * 		+ (5 -> 6 -> 4) 
  * Output: 7 -> 0 -> 8
+ * 342 + 465 = 798
+ * 加法笔算的过程
  */
 public class Q2 {
 	
-	public class ListNode {
+	public static class ListNode {
 		int val;
 		ListNode next;
 
@@ -24,12 +25,46 @@ public class Q2 {
 		}
 	}
     public static ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        return null;
+    	ListNode result = null;
+        ListNode next = null;
+        int carry = 0;
+        do{
+        	int sum = (null == l1? 0 : l1.val) 
+        			+ (null == l2? 0 : l2.val);
+        	int real = (sum + carry) % 10;
+        	carry = (sum + carry) / 10;
+        	if(null == result){
+        		result = new ListNode(real);
+        	}else if(null == next){
+        		next = new ListNode(real);
+        		result.next = next;
+        	}else{
+        		next.next = new ListNode(real);
+        		next = next.next;
+        	}
+        	l1 = null == l1? null : l1.next;
+        	l2 = null == l2? null : l2.next;
+        	
+        }while(null != l1 || null != l2 || carry != 0);
+        return result;
     }
     
 	@Test
 	public void test() {
-		fail("Not yet implemented");
+		ListNode l1 = new ListNode(9);
+		l1.next = new ListNode(9);
+//		l1.next.next = new ListNode(3);
+//		l1.next.next.next = new ListNode(3);
+		
+		ListNode l2 = new ListNode(1);
+//		l2.next = new ListNode(6);
+		
+		ListNode result = Q2.addTwoNumbers(l1, l2);
+		
+		do{
+			System.out.println(result.val);
+			result = result.next;
+		}while(null != result);
 	}
 
 }

40603467d7fa326695a1dcf799d9bdc58189d898
Impler <704090860@qq.com>
2017-02-16 10:06:40 +0800

    solved Q3

---
+++ LeetCode/src/main/java/com/study/leetcode/Q3.java
@@ -0,0 +1,46 @@
+package com.study.leetcode;
+
+import java.util.LinkedList;
+
+import junit.framework.Assert;
+
+import org.junit.Test;
+
+/**
+ * Given a string, find the length of the longest substring without repeating characters.
+ * Examples:
+ * Given "abcabcbb", the answer is "abc", which the length is 3.
+ * Given "bbbbb", the answer is "b", with the length of 1.
+ * Given "pwwkew", the answer is "wke", with the length of 3. 
+ * Note that the answer must be a substring, "pwke" is a subsequence and not a substring.
+ */
+public class Q3 {
+
+	public static int lengthOfLongestSubstring(String s) {
+		if(null == s || s.length() == 0){
+			return 0;
+		}
+		LinkedList<Character> candidate = new LinkedList<Character>();
+		int maxLength = 0;
+		for(int i=0; i<s.length(); i++){
+			char c = s.charAt(i);
+			if(candidate.contains(c)){
+				int index = candidate.indexOf(c);
+				while(index-- >= 0){
+					candidate.removeFirst();
+				}
+			}
+			candidate.add(c);
+			maxLength = Math.max(candidate.size(), maxLength);
+		}
+        return maxLength;
+    }
+	
+	@Test
+	public void test() {
+		Assert.assertEquals(1, Q3.lengthOfLongestSubstring("aaaaa"));
+		Assert.assertEquals(4, Q3.lengthOfLongestSubstring("abcdabad"));
+		Assert.assertEquals(10, Q3.lengthOfLongestSubstring("d123456789"));
+		Assert.assertEquals(5, Q3.lengthOfLongestSubstring("asdiioasd"));
+	}
+}

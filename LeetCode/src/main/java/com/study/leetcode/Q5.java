eaa4c2ced945f8ca95e1657187de817f31fe4700
Impler <704090860@qq.com>
2017-02-20 17:53:34 +0800

    Q5 question

---
+++ LeetCode/src/main/java/com/study/leetcode/Q5.java
@@ -0,0 +1,25 @@
+package com.study.leetcode;
+
+import org.junit.Test;
+/**
+ * Given a string s, find the longest palindromic substring in s. You may assume that the maximum length of s is 1000.
+ * Example:
+ * Input: "babad"
+ * Output: "bab"
+ * Note: "aba" is also a valid answer.
+ * Example:
+ * Input: "cbbd"
+ * Output: "bb"
+ */
+public class Q5 {
+
+    public static String longestPalindrome(String s) {
+    	return null;
+    }
+    
+	@Test
+	public void test() {
+
+	}
+
+}
